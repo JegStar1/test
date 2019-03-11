577c339291585ef2e942879157cb1c4b8935a9a7
Jocke <joakim.olofsson@elev.ga.ntig.se>
2019-03-11 15:26:13 +0100

    test2

---
+++ Övning12.java
@@ -0,0 +1,20 @@
+package jeg;
+
+//* Jocke Olofsson övning 5.3 *//
+import javax.swing.JOptionPane;
+
+public class Övning12 {
+
+	public static void main(String[] args) {
+		int daglön = 1;
+		int dagar = 0;
+		int totalLön = 0;
+		while (totalLön < 1000000000) {
+			dagar += 1;
+			daglön *= 2;
+			totalLön += daglön;
+		}
+		JOptionPane.showMessageDialog(null, "Antal Dagar " + dagar);
+	}
+
+}
